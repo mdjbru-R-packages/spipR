dc9282123ece9e25a2e6eeeda9033547f584e1ff
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2014-09-30 15:00:34 +0300

    Add sleep time in spipRunSim to obtain different random seeds

--- spipR/R/spipRunSim.R
+++ spipR/R/spipRunSim.R
@@ -7,6 +7,8 @@
 #' @param ref_genotypes Used for allele frequencies. A data frame with
 #'   columns working by pairs. Every successive pair of columns represents the
 #'   alleles for one locus. Rows are individuals.
+#' @param sleep_time sleep time at the end of a Spip run. In order to ensure 
+#'   different seeds between runs, it should be greater or equal to 3.
 #'
 #' @return The input parameters and the output from Spip
 #'
@@ -39,7 +41,7 @@
 #'   gtyps_for_all = 50)
 #'
 #' # generate many simulations
-#' n = as.list(1:100)
+#' n = as.list(1:10)
 #' sims = lapply(n, function(x) {
 #'   spipRunSim(ref_genotypes = genotypes,
 #'              max_age = 4,
@@ -54,13 +56,14 @@
 #'              fixed_cohort_size = T,
 #'              cohort_size = 500,
 #'              discard_parents = 4,
-#'              gtyps_for_all = 50)
+#'              gtyps_for_all = 50,
+#'              sleep_time = 0)
 #' })
 #' # check their random seeds
-#' seeds = lapply(sims, function(x) {
+#' seeds = unlist(lapply(sims, function(x) {
 #'    c = grepl("RAND", x$output)
-#'  return(x$output[c])
-#' })
+#'  return(x$output[c][1])
+#' }))
 #'
 #' # This shows that the seeds are the same for runs performed during a single
 #' # second.
@@ -81,7 +84,8 @@ spipRunSim = function(ref_genotypes,
   fixed_cohort_size = TRUE,
   cohort_size,
   discard_parents,
-  gtyps_for_all) {
+  gtyps_for_all,
+  sleep_time = 3) {
 
   library(uuid)
   f = paste0("spipR-tempfile-", UUIDgenerate())
@@ -110,43 +114,15 @@ spipRunSim = function(ref_genotypes,
            gtyps_for_all = gtyps_for_all,
            file = paste0(f, ".gtyp"))
   # run
-  #warning("\"spip_seeds\" was removed (if any file like this existed). This means Spip will use the clock to initialize the seeds. This might cause problems if Spip is run simultaneously or in a very short time (similar seeds between simulations).")
+  file.remove("spip_seeds")
+  warning("\"spip_seeds\" was removed (if any file like this existed). This means Spip will use the clock to initialize the seeds. This might cause problems if Spip is run simultaneously or in a very short time (similar seeds between simulations). Using a sleep_time greater or equal to 3 might be enough.")
   c = paste("spip", "--command-file", paste0(f, ".demog"),
     "--command-file", paste0(f, ".gtyp"))
   print(c)
   output = system(c, intern = TRUE)
-
-  # check the seeds
-  if (sum(grepl("Using clock to set RNG", output)) == 1) {
-    # the clock was used, get those values for the next one
-    seeds = output[grepl("Using clock to set RNG", output)]
-    seeds = strsplit(seeds, split = "Using clock to set RNG: ")[[1]][2]
-    seeds = as.numeric(strsplit(seeds, " ")[[1]])
-    seeds = seeds + 1
-    seeds[1] = seeds[1] + floor((10 ** sample(2:6, 1)) * runif(1))
-    seeds = as.character(seeds)
-    seeds = paste(seeds, collapse = " ")
-    fseeds = file("spip_seeds", "w")
-    cat(seeds, file = fseeds)
-    close(fseeds)
-    #warning(paste0("\"spip_seeds\" overwritten with ", seeds))
-  } else {
-    stopifnot(sum(grepl("Using contents of spip_seeds to set RNG",
-                        output)) == 1)
-    # the spip_seeds file was used
-    seeds = output[grepl("Using contents of spip_seeds to set RNG", output)]
-    seeds = strsplit(seeds,
-      split = "Using contents of spip_seeds to set RNG: ")[[1]][2]
-    seeds = as.numeric(strsplit(seeds, " ")[[1]])
-    seeds = seeds + 1
-    seeds[1] = seeds[1] + floor((10 ** sample(2:6, 1)) * runif(1))
-    seeds = as.character(seeds)
-    seeds = paste(seeds, collapse = " ")
-    fseeds = file("spip_seeds", "w")
-    cat(seeds, file = fseeds)
-    close(fseeds)
-    #warning(paste0("\"spip_seeds\" overwritten with ", seeds))    
-  }
+  file.remove("spip_seeds")
+  # wait 2 sec to ensure that the clock seeds will be different
+  Sys.sleep(sleep_time)
   
   # delete files
   for (e in c(".locpars", ".demog", ".gtyp")) {
